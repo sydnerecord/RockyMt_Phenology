3e3a2baf955abf1dd71b404316f8baf6c21e8783
Sydne Record (she/her/hers) <sydnerecord@users.noreply.github.com>
2026-04-01 13:41:21 -0400

    Visualize Shijia's spatial data
    added code to load hebarium data and shapefile of Rocky Mt. Determined extext of area for MODIS data pull off of EarthData portal

--- Snowmelt_Rasters.R
+++ Snowmelt_Rasters.R
@@ -16,9 +16,29 @@ data_path <- file.path('G:/Shared drives/MSB_Phenology/RockyMt_Phenology/input_d
 
 # Load necessary packages 
 library(sf)
+library(tidyverse)
 
 #Read in Rocky Mt shapefile
 RM_shape <- read_sf(file.path(data_path,"RockyMtShapefile/rocky-mountain-range-area_838-polygon.shp"))
 
 # Read in herbarium data
 herbarium <- read.csv(file.path(data_path,'data_phenology_herbarium.csv'))
+
+# Determine projection of RM shapefile
+st_crs(RM_shape)
+
+# Convert longitude & latitude columns from herbarium data .csv into spatial object for visualization
+sf_points <- st_as_sf(herbarium, coords = c("Longitude", "Latitude"), crs = 4326)
+
+# Visualize herbarium sampling points on RM shapefile to ensure that shapefile is appropriate for cropping MODIS data
+# Plot shapefile first (use $geometry to plot only the shapes, not attributes)
+plot(st_geometry(RM_shape))
+
+# Overlay points
+plot(st_geometry(sf_points), add = TRUE, col = "blue", pch = 19)
+
+# Determine extent of RM shapefile to determine extent of MODIS data to retrieve
+st_bbox(RM_shape)
+
+#xmin       ymin       xmax       ymax 
+#-125.84455   35.37196 -104.42414   60.07528 
